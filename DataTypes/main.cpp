9c8fd981840945d2d755bb21070ed1d32f03f2c7
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2022-10-18 10:46:57 +0200

    Add files via upload

---
+++ DataTypes/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    string k= "Kevin";
+    int k1= 4;
+    char k2= 'K';
+    float k4=3.14;
+    vector<int>v1(10)={1,2,3,4,5,6,7,8,9,10};
+    vector<string>v2(10)={"Kevin","Is","fifteen","years"};
+    int array1[10]={1,2,3,4,5,6,7,8,9,10};
+    char array2[10]={'k','e','v','i','n'};
+    float array3[10]= {3.14,3.15,3.16,3.17};
+    bool test=0; //true or false
+    bool test=1; //true or false
+
+}
